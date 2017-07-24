bdf0bf86b44db25be6325c8eac3e187804162e40
Donny Gabai <mrdgabai@gmail.com>
2017-07-24 15:53:11 -0400

    first

---
+++ typography.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    // $("button").click(function(){
+    //     var div = $("#clean");
+    //    $("#clean").animate({left: '300px', opacity: '0.5'}, "slow");
+        
+    // });
+});
