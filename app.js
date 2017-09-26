bbf172e216fcd0ddf4230ca9f68a3735b407d76c
Donny Gabai <mrdgabai@gmail.com>
2017-09-26 19:38:48 -0400

    more to come

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // $(function() {
+    //         $('.container').click(function() {
+    //             $('.container').toggleClass('open');
+    //         });
+    //     })
+    // $("button").click(function(){
+    //     var div = $("#clean");
+    //    $("#clean").animate({left: '300px', opacity: '0.5'}, "slow");
+
+    // });
+});
--- typography.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    // $("button").click(function(){
-    //     var div = $("#clean");
-    //    $("#clean").animate({left: '300px', opacity: '0.5'}, "slow");
-        
-    // });
-});
